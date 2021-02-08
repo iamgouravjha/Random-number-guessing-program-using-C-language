584cb7923350adee411f49b6a9c0a18475ca0327
Gourav Jha <76039520+iamgouravjha@users.noreply.github.com>
2021-02-08 17:17:29 +0530

    updates regards the presentation.

--- game.c
+++ game.c
@@ -5,7 +5,7 @@ int main(){
     int number, guess, guessno=1;
     srand(time(0));
     number= rand()%100 + 1; //Generate a random number between 1 to 100.
-    printf("The number is %d\n",number);
+    // printf("The number is %d\n",number);
     //keep running the loop until the number is gussed.
     do
     {
@@ -13,15 +13,15 @@ int main(){
         scanf("%d",&guess);
         if (guess>number)
         {
-            printf("Enter a lower number\n");
+            printf("Enter a lower number\n\n");
         }
         else if (guess<number)
         {
-            printf("Enter a greater number\n");
+            printf("Enter a greater number\n\n");
         }
         else
         {
-            printf("You guessed the correct no.%d in %d attempts",number,guessno);
+            printf("\nCorrect! You guessed the correct no.%d in %d attempts.\n",number,guessno);
         }
         guessno++;
         
